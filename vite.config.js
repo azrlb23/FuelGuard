5be8b1709b5ed9e799e76cde13a91e3c9bde7318
perhanjay <11241056@student.itk.ac.id>
2026-08-08 00:09:29 +0800

    change: changing vite config to drop all console logs for build, change pwa desc to fuelguard (before is habi jaya)

--- vite.config.js
+++ vite.config.js
@@ -7,31 +7,34 @@ import tailwindcss from '@tailwindcss/vite'
 import { VitePWA } from 'vite-plugin-pwa'
 
 export default defineConfig({
+  esbuild: {
+    drop: ['console', 'debugger']
+  },
   plugins: [
     vue(),
     vueDevTools(),
-    
-    tailwindcss(), 
+
+    tailwindcss(),
 
     VitePWA({
-      registerType: 'autoUpdate', 
-      includeAssets: ['favicon.ico', 'apple-touch-icon.png'], 
+      registerType: 'autoUpdate',
+      includeAssets: ['favicon.ico', 'apple-touch-icon.png'],
       manifest: {
-        name: 'Habi Jaya Management System', 
-        short_name: 'Habi Jaya', 
-        description: 'Sistem Manajemen Operasional SPBU Habi Jaya',
-        theme_color: '#143d2e', 
-        background_color: '#f5f5f5', 
-        display: 'standalone', 
-        orientation: 'portrait', 
+        name: 'FuelGuard Management System',
+        short_name: 'FuelGuard',
+        description: 'Sistem Manajemen Pengetap SPBU FuelGuard',
+        theme_color: '#143d2e',
+        background_color: '#f5f5f5',
+        display: 'standalone',
+        orientation: 'portrait',
         icons: [
           {
-            src: 'pwa-192x192.png', 
+            src: 'pwa-192x192.png',
             sizes: '192x192',
             type: 'image/png'
           },
           {
-            src: 'pwa-512x512.png', 
+            src: 'pwa-512x512.png',
             sizes: '512x512',
             type: 'image/png'
           },
@@ -39,13 +42,13 @@ export default defineConfig({
             src: 'pwa-512x512.png',
             sizes: '512x512',
             type: 'image/png',
-            purpose: 'any maskable' 
+            purpose: 'any maskable'
           }
         ]
       },
       workbox: {
         globPatterns: ['**/*.{js,css,html,ico,png,svg,jpg,jpeg}'],
-        maximumFileSizeToCacheInBytes: 3000000 
+        maximumFileSizeToCacheInBytes: 3000000
       }
     })
   ],
